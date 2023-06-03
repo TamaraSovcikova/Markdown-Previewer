7906f544962f31477cf079832cb39156187a27bb
snaccident <tamara@sovcik.net>
2023-06-03 13:45:01 +0100

    slight tweeking

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,35 +1,43 @@
-import './App.css';
-import marked from 'marked';
+import './index.css';
+//import marked from 'marked';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const markdownText = "# Hello there";
+//const markdownText = "# Hello there";
 
-const MarkdownComponent = (markdownText) => {  
-  const html = marked(markdownText);
+// const MarkdownComponent = (markdownText) => {  
+//   const html = marked(markdownText);
 
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-};
+//   return <div dangerouslySetInnerHTML={{ __html: html }} />;
+// };
 
 
 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="container">
+                <h2>Column 1</h2>
+                <p>Content of column 1</p>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <div className="container">
+                <h2>Column 2</h2>
+                <p>Content of column 2</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 } 
 
-export { App, MarkdownComponent };
+export default App;
