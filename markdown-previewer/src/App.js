b24f8f955bf38bbc117a60fd49ccc5155350f3bd
snaccident <tamara@sovcik.net>
2023-06-07 12:42:41 +0100

    interpreting <br>

--- markdown-previewer/src/App.js
+++ markdown-previewer/src/App.js
@@ -1,21 +1,21 @@
 import marked from 'marked';
 import './index.css';
 import React from 'react';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun, faTint } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
-  const placeholderText = 
-  `# (づ ◕‿◕ )づ 
-## Welcome to the Markup Previewer
+  const placeholderText = `# (づ ◕‿◕ )づ
+  ## Welcome to the Markup Previewer
 ##### This is a project for the Front End Development Libraries Certificate
-[Link](https://freecodecamp.org)
+
+[freecodecamp](https://freecodecamp.org)<br><br>
 
 Inline Code:
-\`console.log("Hello, There!");\`
-
+\`console.log("Pleasure to see u here!");\`
+<br>
 Code block:
 \`\`\`
 function greet(name) {
@@ -32,20 +32,36 @@ This project was made with:
 > Blockquote: "Be yourself; everyone else is already taken." - Oscar Wilde
 
 ![Image](https://mystickermania.com/cdn/stickers/cute-cats/cat-sunglasses-512x512.png)
+**Thank you for visiting!!**`;
 
-**Thank you for visiting!!**
-`;
-const markdownText = (markdownText) => {  
-  const html = marked(markdownText);
+
+
+const markdownText = (markdownText) => {
+  const renderer = new marked.Renderer();
+
+  renderer.paragraph = (text) => {
+    return text.replace(/\n/g, '<br>'); 
+  };
+
+  renderer.codespan = (code) => {
+    return `<code>${code}</code>`; 
+  };
+
+  renderer.code = (code, language) => {
+    return `<pre><code>${code}</code></pre>`;
+  };
+
+  const html = marked(markdownText, { renderer });
 
   return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
-const [editorValue, setEditorValue ] = useState(placeholderText);
-const [previewValue, setPreviewValue] = useState(markdownText(editorValue));
 
 
-const handleEditorChange = (event) =>{
-  const newValue = event.target.value;  
+const [editorValue, setEditorValue] = useState(placeholderText);
+const [previewValue, setPreviewValue] = useState(markdownText(placeholderText));
+
+const handleEditorChange = (event) => {
+  const newValue = event.target.value;
   setEditorValue(newValue);
   setPreviewValue(markdownText(newValue));
 };
@@ -71,10 +87,10 @@ useEffect(() => {
       h2Color: 'black',
       buttonColor: 'black',
       buttonIcon: '#e1dfe1',
-      },
+    },
     aqua: {
       backgroundColor: '#b9d0e1',
-      h2Color: '#445066',     
+      h2Color: '#445066',
       buttonColor: '#e1dfe1;',
       buttonIcon: '#454545',
     },
@@ -87,57 +103,44 @@ useEffect(() => {
   });
 }, [theme]);
 
-  
-
-  return (    
-   <div className="App">
-      <div className="container-fluid">
-        <div className="row no-gutters">
-          <div className="col-1 pl-0  d-flex align-items-center justify-content-center">
-            <div className="theme-buttons">
-              <button onClick={() => toggleTheme('light')} className="btn btn-light">
-                <FontAwesomeIcon icon={faSun} />
-              </button>
-              <button onClick={() => toggleTheme('dark')} className="btn btn-dark">
-                <FontAwesomeIcon icon={faMoon} />
-              </button>
-              <button onClick={() => toggleTheme('aqua')} className="btn btn-info">
-                <FontAwesomeIcon icon={faTint} />
-              </button>
-            </div>
+return (
+  <div className="App">
+    <div className="container-fluid">
+      <div className="row no-gutters">
+        <div className="col-1 pl-0  d-flex align-items-center justify-content-center">
+          <div className="theme-buttons">
+            <button onClick={() => toggleTheme('light')} className="btn btn-light">
+              <FontAwesomeIcon icon={faSun} />
+            </button>
+            <button onClick={() => toggleTheme('dark')} className="btn btn-dark">
+              <FontAwesomeIcon icon={faMoon} />
+            </button>
+            <button onClick={() => toggleTheme('aqua')} className="btn btn-info">
+              <FontAwesomeIcon icon={faTint} />
+            </button>
           </div>
-          <div className="col-4">
-            <div className="half-screen-container">
-              <h2 className='head'>Editor</h2>
-              <textarea
-                id="editor"
-                className="half-screen-textarea editor"
-                value={editorValue}
-                onChange={handleEditorChange}
-              />
-            </div>
+        </div>
+        <div className="col-4">
+          <div className="half-screen-container">
+            <h2 className="head">Editor</h2>
+            <textarea
+              id="editor"
+              className="half-screen-textarea editor"
+              value={editorValue}
+              onChange={handleEditorChange}
+            />
           </div>
-          <div className="col-7">
-            <div className="half-screen-container">
-              <h2 className='head'>Preview</h2>
-              <div className="half-screen-textarea preview">{previewValue}</div>
-            </div>
+        </div>
+        <div className="col-7">
+          <div className="half-screen-container">
+            <h2 className="head">Preview</h2>
+            <div className="half-screen-textarea preview">{previewValue}</div>
           </div>
         </div>
       </div>
     </div>
-
-
-
-
-
-
-
-  );
+  </div>
+);
 }
 
 export default App;
-
-
-
-
